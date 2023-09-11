4229dc9cfa84c7445c2e3605446f51600129ea1a
vurajyothisri <vurajyothisri@gmail.com>
2023-09-11 18:52:42 +0530

    commit

---
+++ Javascript project/Guessword/script.js
@@ -0,0 +1,7 @@
+let meassage=document.querySelector("#guess_meassage")
+let word_lines=document.querySelector("#word")
+let guess=document.querySelector("#Guess")
+let entered_letters=document.querySelector("#Entered_letters")
+
+word=["cloud","crab","psychology","science","foot","library","pricipal","priciple","school","cool","power","letters","company"]
+console.log("hello");
