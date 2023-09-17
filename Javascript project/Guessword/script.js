9337d0eac739ca1a21e4c8ba946050b3438dd4e7
vurajyothisri <vurajyothisri@gmail.com>
2023-09-17 11:28:55 +0530

    button disability

--- Javascript project/Guessword/script.js
+++ Javascript project/Guessword/script.js
@@ -1,28 +1,26 @@
-
 let meassage=document.querySelector("#guess_meassage") //print y is there or not 
-let word_lines=document.querySelector("#Word") // .it is a container for word
-let guess=document.querySelector("#Guess") //chances
+let word_lines=document.querySelector("#Word") //it is a container for word
+let guess=document.querySelector("#Guess") //Chances
 let entered_letters=document.querySelector("#Entered_letters") //user entered words
-let letter_messsage=document.querySelector("#no_of_letters")//print dashes of the word (length)
+let letter_messsage=document.querySelector("#no_of_letters")//print no of dashes of the word (length)
 let error_message=document.querySelector("#Error_message")// to print error meassage
 let button=document.querySelector("#Submition")
-let correct_list=[]
 let entered_list=[]
+var guess_count=3
 word=["cloud","crab","psychology","science","foot","library","pricipal","priciple","school","cool","power","letters","company"]
 
 random_word=word[Math.floor(Math.random()*word.length)]
 console.log(random_word);
-lines_list=[]
-// for (let i=0;i<random_word.length;i++){
-//     lines_list[i]='_ '
-// }
-//writing how may letters are ther in words
-letter_messsage.innerHTML=` You word has ${random_word.length} letters`
-for(let j=0;j<random_word.length;j++){
-    let line=" _ "
-    word_lines.append(line)  
-
+var lines_list=[]
+for (let i=0;i<random_word.length;i++){
+    lines_list[i]='_ '
 }
+//writing how may letters are ther in words
+letter_messsage.innerHTML=` You word has ${random_word.length} letters`;
+for (let i=0;i<random_word.length;i++){
+    console.log(lines_list[i])
+}
+word_lines.innerHTML="_ ".repeat(random_word.length)
 
 //To find given input is alphabet or not
 function letterornot(letter){
@@ -32,10 +30,13 @@ function letterornot(letter){
 function ErrorMessage(Str){
     if(Str.length>1){
         error_message.innerHTML= `You Entered ${Str.length} Alphabtes.Enter One Alphabet`
+      
         return false
   }
   else if(Str.length===0){
-    error_message.innerHTML="Please Enter the Alphabet"
+        error_message.innerHTML="Please Enter the Alphabet"
+       
+
     return false
   }
   
@@ -50,39 +51,64 @@ function ErrorMessage(Str){
   }
 
 }
+
+
+
 //clearing the error_message while entering in the inputfield
-
-
 button.addEventListener('click',()=>{
     word_lines.innerHTML=""
-    let input=document.querySelector('#input_value').value
-    console.log("heloo")
-    if(ErrorMessage(input)){
-        if(random_word.includes(input)){
-            console.log("heloo")
-           for(let i=0;i<random_word.length;i++){
-            if(random_word[i]==input){
-                // lines_list[i]=input
-                // console.log(`${lines_list[i]}`)
-                word_lines.append(input)  
-
-            }
-            else{
-                let line="   _ "
-                word_lines.append(line)  
-            }
-           }
-        //    if(!entered_list.includes(input)){
-        //     console.log(!entered_list.includes(input));
-        //     entered_list.unshift(input)
-        //    }
-        //    console.log(!entered_list.includes(input));
-        // word_lines.textContent = lines_list;
-        }
+    let input=document.querySelector('#input_value')
+    if(ErrorMessage(input.value))
+    {
+         
+          if(random_word.includes(input.value))
+          { 
+               letter_messsage.innerHTML=`Good Guess the word has the letter ${input.value} `
+               for(let i=0;i<random_word.length;i++)
+               {
+                  if(random_word[i]==input.value)
+                  {
+                    lines_list[i]=input.value+"  "
+                  }
+               }      
+          }
+         else
+         {
+          letter_messsage.innerHTML=`Wrong Guess the word  doesn't have the letter ${input.value} `
+            guess_count=guess_count-1
+            guess.innerHTML=guess_count
+         }
+    }
+    // To append the words that entered in the ipnput field
+    for(let i=0;i<lines_list.length;i++)
+    {
+      word_lines.append(lines_list[i])
     }
    
+   
+  
 })
-for(let j=0;j<entered_list.length;j++){
-    console.log(entered_list[j])
-}
 
+console.log(entered_list)
+
+//To disabled button after Guess_count is zero
+button.addEventListener('click',()=>{
+  
+  if (guess_count===0){
+    button.disabled=true
+    letter_messsage.innerHTML="You lost the game.Sorry if you want to continue refresh the page"
+
+  }
+})  
+button.addEventListener('click',()=>{
+  let input=document.querySelector('#input_value')
+  if(!entered_list.includes(input.value))
+  {
+         entered_list.push(input.value)
+  }
+  else
+  {
+    letter_messsage.innerHTML="You already entered that word"
+  }
+  input.value=""
+})
